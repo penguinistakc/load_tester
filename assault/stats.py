5d2d66581e36a5aa37ed90879333c03c82cf8605
Matthew Copple <mcopple@gmail.com>
2019-09-08 02:20:31 +0000

    slowest method fleshed out for stats

---
+++ assault/stats.py
@@ -0,0 +1,96 @@
+from typing import List, Dict
+
+
+class Results:
+    """
+    Results handles calculating statistics based on a list of requests that were made.
+
+    --- Results ---
+    Successful requests     3000
+    Slowest                 0.010s
+    Fastest                 0.001s
+    Average                 0.003s
+    Total time              2.400s
+    Requests Per Minute     90000
+    Requests Per Second     1250
+    """
+
+    def __init__(self, total_time: float, requests: List[Dict]):
+        self.total_time = total_time
+        self.requests = sorted(requests, key=lambda r: r["request_time"])
+
+    def slowest(self) -> float:
+        """
+        Returns the slowest request's completion time
+
+        >>> results = Results(10.6, [{
+        ...     'status_code': 200,
+        ...     'request_time': 3.4,
+        ... }, {
+        ...     'status_code': 500,
+        ...     'request_time': 6.1,
+        ... }, {
+        ...     'status_code': 200,
+        ...     'request_time': 1.04,
+        ... }])
+        >>> results.slowest()
+        6.1
+        """
+        return self.requests[-1]["request_time"]
+
+    def fastest(self) -> float:
+        """
+        Returns the fastest request's completion time
+
+        >>> results = Results(10.6, [{
+        ...     'status_code': 200,
+        ...     'request_time': 3.4,
+        ... }, {
+        ...     'status_code': 500,
+        ...     'request_time': 6.1,
+        ... }, {
+        ...     'status_code': 200,
+        ...     'request_time': 1.04,
+        ... }])
+        >>> results.fastest()
+        1.04
+        """
+        pass
+
+    def average_time(self) -> float:
+        """
+        Returns the average request's completion time
+
+        >>> results = Results(10.6, [{
+        ...     'status_code': 200,
+        ...     'request_time': 3.4,
+        ... }, {
+        ...     'status_code': 500,
+        ...     'request_time': 6.1,
+        ... }, {
+        ...     'status_code': 200,
+        ...     'request_time': 1.04,
+        ... }])
+        >>> results.average_time()
+        9.846666667
+        """
+        pass
+
+    def successful_requests(self) -> int:
+        """
+        Returns the number of successful requests
+
+        >>> results = Results(10.6, [{
+        ...     'status_code': 200,
+        ...     'request_time': 3.4,
+        ... }, {
+        ...     'status_code': 500,
+        ...     'request_time': 6.1,
+        ... }, {
+        ...     'status_code': 200,
+        ...     'request_time': 1.04,
+        ... }])
+        >>> results.successful_requests()
+        2
+        """
+        pass
